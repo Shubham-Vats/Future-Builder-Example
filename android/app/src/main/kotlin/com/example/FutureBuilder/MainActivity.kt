2710fa205acac98617c895078618abcdd795142d
Shubham-Vats <vats.shubham.99@gmail.com>
2020-11-03 01:25:40 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/FutureBuilder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.FutureBuilder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+List data;
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  Future getData() async{
+    String url = "https://jsonplaceholder.typicode.com/users";
+    try {
+      http.Response response = await http.get(url);
+      if(response.statusCode ==200) {
+        print("Data Fetched Successfully");
+        data = json.decode(response.body);
+        return data;
+      } 
+    }catch (e) {
+      print(e);
+      print("No Data Fetched");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FutureBuilder", style: TextStyle(color: Colors.black),),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 10.0,
+      ),
+      body: Container(
+        color: Colors.redAccent,
+        child: FutureBuilder(
+          future: getData(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if(snapshot.hasData){
+              return ListView.builder(
+                itemCount: snapshot.data.length == null ? 0 : snapshot.data.length,
+                itemBuilder: (context, index){
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      elevation: 10.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: <Widget>[
+                            Text("ID: "+snapshot.data[index]["id"].toString()),
+                            Text("Name: "+snapshot.data[index]["name"]),
+                            Text("UserName: "+snapshot.data[index]["username"]),
+                            Text("E-Mail: "+snapshot.data[index]["email"]),
+                            SizedBox(height: 20.0),
+                            Text("Street: "+snapshot.data[index]["address"]["street"]),
+                            Text("Suite "+snapshot.data[index]["address"]["suite"]),
+                            Text("City: "+snapshot.data[index]["address"]["city"]),
+                            Text("ZipCode: "+snapshot.data[index]["address"]["zipcode"].toString()),
+                            SizedBox(height: 20.0),
+                            Text("Geo: "+snapshot.data[index]["address"]["geo"].toString()),
+                            SizedBox(height: 20.0),
+                            Text("Phone: "+snapshot.data[index]["phone"].toString()),
+                            Text("Website: "+snapshot.data[index]["website"]),
+                            SizedBox(height: 20.0),
+                            Text("Company Name: "+snapshot.data[index]["company"]["name"]),
+                            Text("Company Phrase: "+snapshot.data[index]["company"]["catchPhrase"]),
+                            Text("Company BS: "+snapshot.data[index]["company"]["bs"]),
+                          ]
+                        ),
+                      ),
+                    ),
+                  );
+                }
+              );
+            }
+            else{
+              return Center
+              (
+                child: Container
+                (
+                  height: 100,
+                  width: 100,
+                  child: CircularProgressIndicator()
+                )
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
